d6593f32bf94489f418d709c948d3278b9cf56bf
cristina <mariacri363@gmail.com>
2020-07-28 21:58:25 -0400

    funciona el idb

--- src/public/cache/add.js
+++ src/public/cache/add.js
@@ -1,75 +1,96 @@
+const indexedDB = window.indexedDB
+const form = document.getElementById('form')
+const tasks = document.getElementById('tasks')
 
-
-//let addButton = document.getElementById("add")
-/*
-let pictures = document.getElementById("tasks")
-
-function updateUI(data) {
-  clearPictures()
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i], "\n") 
-    createImage(data[i])
+if(indexedDB && form){
+  let db 
+  const request = indexedDB.open('listaTareas', 1)
+  request.onsuccess = () => {
+    db = request.result
+    console.log('OPEN', db)
+    addbdData()
+    readData()
   }
-}
-
-
-function clearPictures() {
-  while(pictures.hasChildNodes()) {
-    pictures.removeChild(pictures.lastChild)
-  }
-}
-
-function createImage(data){
-  let imageBlock = document.createElement('div')
-  imageBlock.className = 'w3-row w3-margin w3-mobile'
-  let imageItem = document.createElement('div')
-  imageItem.className = 'w3-third'
-  let img = document.createElement('img')
-  img.src = data.imageurl
-
-  let textItem = document.createElement('div')
-  textItem.className = 'w3-twothird w3-container w3-mobile'
-  let title = document.createElement('h2')
-  title.textContent = data.title
-  let text = document.createElement('p')
-  text.innerHTML = 'source: ' + data.text
-
-  imageBlock.appendChild(imageItem)
-  imageBlock.appendChild(textItem)
-  imageItem.appendChild(img)
-  textItem.appendChild(title)
-  textItem.appendChild(text)
-
-  pictures.appendChild(imageBlock)
-}
-*/
-
-let url = 'http://localhost:3000/api/task'
-let networkDataReceived = false
-
-fetch(url)
-  .then(function(res) {
-    //  console.log('lleog aqui')
-    return res.json()
-  })
-  .catch(err => console.error(err))
-  .then(function(data) {
-    networkDataReceived = true
-    console.log('From web', data)
-    let dataArray = [];
-    for (let key in data) {
-      dataArray.push(data[key])
-    }    
-  })
-  .catch(err => console.error(err))
-
-
-if ('indexedDB' in window) {
-    readAllData('items')
-    .then(function(data) {
-      if (!networkDataReceived) {
-        console.log('From idb', data)
-        //updateUI(data)
-      }
+  request.onupgradeneeded = () => {
+    db = request.result
+    console.log('Create', db)
+    const objectStore = db.createObjectStore('Tareas', {
+      autoIncrement: true
+      //keyPath: 
     })
+  }
+
+  request.onerror = (error) => {
+    console.log('Error', error)
+  }
+
+  const addData = (data) => {
+    const transaction = db.transaction(['Tareas'], 'readwrite')
+    const objectStore = transaction.objectStore('Tareas')
+    const request = objectStore.add(data)
+    readData()
+  }
+  //recoge de la bd
+  const url = 'http://localhost:3000/api/task'
+    fetch(url)
+        .then(function(res) {
+          //  console.log('lleog aqui')
+          return res.json()
+        })
+        .catch(err => console.error(err))
+        .then(function(data) {
+          networkDataReceived = true
+          //console.log('From web', data)
+          let dataArray = [];
+          for (let key in data) {
+            dataArray.push(data[key])
+            addbdData(data[key])
+            //console.log('From web', data[2])
+          }    
+        })
+        .catch(err => console.error(err))
+
+  const addbdData = (data) => {
+    const transaction = db.transaction(['Tareas'], 'readwrite')
+    const objectStore = transaction.objectStore('Tareas')
+    const request = objectStore.add(data)
+    readData()
+  }
+
+  const readData = () => {
+    const transaction = db.transaction(['Tareas'], 'readonly')
+    const objectStore = transaction.objectStore('Tareas')
+    const request = objectStore.openCursor()
+    const fragment = document.createDocumentFragment()
+    //addbdData()
+ 
+    request.onsuccess = (e) => {
+      const cursor = e.target.result
+      //console.log(e.target)
+      if(cursor){
+        //console.log(cursor.value)
+        const taskTitle = document.createElement('P')
+        taskTitle.textContent = cursor.value.taskTitle
+        fragment.appendChild(taskTitle)
+        const taskDescription = document.createElement('P')
+        taskDescription.textContent = cursor.value.taskDescription
+        fragment.appendChild(taskDescription)
+        cursor.continue()
+      } else {
+        tasks.textContent = ''
+        tasks.appendChild(fragment)
+        //console.log('No hay mas datos')
+      }
+    }
+  }
+
+  form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     const data = {
+       taskTitle: e.target.title.value,
+       taskDescription: e.target.description.value
+     }
+     //console.log(data)
+     addData(data)
+  })
 }
--- src/public/sw.js
+++ src/public/sw.js
@@ -1,5 +1,5 @@
 console.log('sw')
-
+/*
 const cacheName = 'cursito'
 
 self.addEventListener('fetch', function(event) {
@@ -31,3 +31,118 @@ self.addEventListener('fetch', function(event) {
     })
   )
 })
+
+*/
+
+importScripts('/cache/idb.js');
+importScripts('/cache/db-utils.js');
+
+let STATIC_CACHE = 'static-cursito'
+let DYNAMIC_CACHE = 'dynamic-cursito'
+let STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/cache/app.js',
+  '/cache/add.js',
+  '/cache/promise.js',
+  '/cache/fetch.js',
+  '/cache/idb.js',
+  '/cache/db-utils.js'
+]
+
+self.addEventListener('install', function (event) {
+  // console.log('[SW] Installing  ...', event)
+  event.waitUntil(
+    caches.open(STATIC_CACHE)
+      .then(function (cache) {
+        // console.log('[SW] App Shell cache ')
+        cache.addAll(STATIC_FILES)
+      }).catch(function (error){ console.log('[SW] App shell cache wrong error!', error)})
+  )
+})
+
+self.addEventListener('activate', function (event) {
+  // console.log('[SW] Activate ....', event)
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE) {
+            // console.log('[SW] clean older cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+  )
+  return self.clients.claim()
+})
+
+function isInArray(string, array) {
+  let cachePath
+  if (string.indexOf(self.origin) === 0) {
+    // console.log('matched ', string)
+    cachePath = string.substring(self.origin.length)
+  } else {
+    cachePath = string
+  }
+  return array.indexOf(cachePath) > -1
+}
+
+self.addEventListener('fetch', function (event) {
+
+  let url = 'http://localhost:3000/api/task'
+
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      fetch(event.request)
+            .then(function (res) {
+              let responseCloned = res.clone()
+
+              clearAllData('items')
+                .then(function () {
+                  return responseCloned.json()
+                })
+                .then(function (data){
+                  for (let key in data) {
+                    writeData('items', data[key])
+                  }
+                })
+
+              return res
+            })
+    )
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    console.log("url req: ",event.request.url)
+    event.respondWith(
+      caches.match(event.request).catch(function (e){console.log('Error: ', e)})
+    )
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response
+          } else {
+            return fetch(event.request)
+              .then(function (res) {
+                return caches.open(DYNAMIC_CACHE)
+                  .then(function (cache) {
+                    cache.put(event.request.url, res.clone())
+                    return res
+                  })
+              })
+              .catch(function (err) {
+                return caches.open(STATIC_CACHE)
+                  .then(function (cache) {
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                      return cache.match('index.html')
+                    }
+                  })
+              })
+          }
+        })
+    )
+  }
+
+})
+
