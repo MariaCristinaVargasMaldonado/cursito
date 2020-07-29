209ff5cecb7dd6357171fde461bce9d452a9fefb
cristina <mariacri363@gmail.com>
2020-07-29 16:30:44 -0400

    fail

---
+++ src/app/routes.js
@@ -0,0 +1,9 @@
+const passport = require("passport");
+
+module.exports = (app, passport) => {
+
+    app.get('/login', (req, res) => {
+        //res.render('index')
+        console.log('sdfsdfsd')
+    })
+}
